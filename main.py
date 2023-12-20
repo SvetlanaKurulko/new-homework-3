5e35b96f99c7f529850eb79e56ed8af5ee5a713c
ПК <svetochka116236@gmail.com>
2023-12-20 13:23:49 +0200

    implemented second task

--- main.py
+++ main.py
@@ -29,3 +29,24 @@
 #
 #     except Exception as e:
 #                 print(f"Error: {e}")
+
+
+
+# Користувач вводить два числа.
+# Визначити, чи рівні ці числа, і, якщо ні,
+# вивести їх на екран у порядку зростання
+
+
+# while True:
+#     try:
+#         num1 = float(input("Введіть перше число: "))
+#         num2 = float(input("Введіть друге число: "))
+#
+#         if num1 == num2:
+#             print("Числа рівні.")
+#         else:
+#             sorted_numbers = sorted([num1, num2])
+#             print("Числа у порядку зростання:", sorted_numbers)
+#
+#     except ValueError:
+#         print("Невірне введення. Будь ласка, введіть числа.")
