8e9cc8a1ded075379fa3e9832c10ed0a1afa6442
ПК <svetochka116236@gmail.com>
2024-01-22 15:43:43 +0200

    Homework nine

--- main6.py
+++ main6.py
@@ -6,28 +6,121 @@
 
 
 
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+# class Teacher(Person):
+#     def __init__(self, name, age, expertise):
+#         super().__init__(name, age)
+#         self.expertise = expertise
+#
+# class Student(Person):
+#     def __init__(self, name, age, courses):
+#         super().__init__(name, age)
+#         self.courses = courses
+#
+# class Subject:
+#     def __init__(self, name, teacher, students):
+#         self.name = name
+#         self.teacher = teacher
+#         self.students = students
+#
+# class Academy:
+#     def __init__(self, name, subjects):
+#         self.name = name
+#         self.subjects = subjects
+
+
+# v2
+
 class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    def __init__(self, name, surname, age):
+        self._name = name
+        self._surname = surname
+        self._age = age
+
+    def display_info(self):
+        print(f"Name: {self._name}, Surname: {self._surname}, Age: {self._age}")
+
 
 class Teacher(Person):
-    def __init__(self, name, age, expertise):
-        super().__init__(name, age)
-        self.expertise = expertise
+    def __init__(self, name, surname, age, subjects_taught):
+        super().__init__(name, surname, age)
+        self._subjects_taught = subjects_taught
+
+    def display_info(self):
+        super().display_info()
+        print(f"Subjects Taught: {', '.join(self._subjects_taught)}")
+
+    def conduct_lesson(self):
+        print(f"Teacher {self._name} is conducting a lesson.")
+
 
 class Student(Person):
-    def __init__(self, name, age, courses):
-        super().__init__(name, age)
-        self.courses = courses
+    def __init__(self, name, surname, age, course, group):
+        super().__init__(name, surname, age)
+        self._course = course
+        self._group = group
+
+    def display_info(self):
+        super().display_info()
+        print(f"Course: {self._course}, Group: {self._group}")
+
+    def do_homework(self):
+        print(f"Student {self._name} is doing homework.")
+
 
 class Subject:
-    def __init__(self, name, teacher, students):
-        self.name = name
-        self.teacher = teacher
-        self.students = students
+    def __init__(self, name, description, teacher, students):
+        self._name = name
+        self._description = description
+        self._teacher = teacher
+        self._students = students
+
+    def display_info(self):
+        print(f"Subject: {self._name}, Description: {self._description}")
+        print("Teacher:")
+        self._teacher.display_info()
+        print("Students:")
+        for student in self._students:
+            student.display_info()
+
 
 class Academy:
-    def __init__(self, name, subjects):
-        self.name = name
-        self.subjects = subjects
+    def __init__(self):
+        self._teachers = []
+        self._students = []
+        self._subjects = []
+
+    def add_teacher(self, teacher):
+        self._teachers.append(teacher)
+
+    def add_student(self, student):
+        self._students.append(student)
+
+    def add_subject(self, subject):
+        self._subjects.append(subject)
+
+    def display_statistics(self):
+        print(f"Number of Teachers: {len(self._teachers)}")
+        print(f"Number of Students: {len(self._students)}")
+        print(f"Number of Subjects: {len(self._subjects)}")
+
+
+# Тестування
+teacher1 = Teacher("John", "Doe", 35, ["Math", "Physics"])
+student1 = Student("Alice", "Smith", 20, 2, "A")
+subject1 = Subject("Mathematics", "Advanced Calculus", teacher1, [student1])
+
+academy = Academy()
+academy.add_teacher(teacher1)
+academy.add_student(student1)
+academy.add_subject(subject1)
+
+teacher1.conduct_lesson()
+student1.do_homework()
+subject1.display_info()
+
+academy.display_statistics()
---
+++ main7.py
@@ -0,0 +1,104 @@
+# 1. Створіть клас "Місто". Необхідно зберігати в полях класу:
+#    назву міста, назву регіону, назву країни, кількість жителів міста,
+#    поштовий індекс міста, телефонний код міста.
+#    Реалізуйте доступ до окремих полів (Інкапсуляція).
+
+
+# class City:
+#     def __init__(self, name, region, country, population, postal_code, phone_code):
+#         self._name = name
+#         self._region = region
+#         self._country = country
+#         self._population = population
+#         self._postal_code = postal_code
+#         self._phone_code = phone_code
+#
+#     def get_name(self):
+#         return self._name
+#
+#     def get_region(self):
+#         return self._region
+#
+#     def get_country(self):
+#         return self._country
+#
+#     def get_population(self):
+#         return self._population
+#
+#     def get_postal_code(self):
+#         return self._postal_code
+#
+#     def get_phone_code(self):
+#         return self._phone_code
+#
+#     def set_name(self, name):
+#         self._name = name
+#
+#     def set_region(self, region):
+#         self._region = region
+#
+#     def set_country(self, country):
+#         self._country = country
+#
+#     def set_population(self, population):
+#         self._population = population
+#
+#     def set_postal_code(self, postal_code):
+#         self._postal_code = postal_code
+#
+#     def set_phone_code(self, phone_code):
+#         self._phone_code = phone_code
+
+
+
+
+# 2. Створіть клас "Країна". Необхідно зберігати в полях класу:
+#    назву країни, назву континенту, кількість жителів країни,
+#    телефонний код країни, назву столиці, назву міст країни.
+#    Реалізуйте доступ до окремих полів (Інкапсуляція).
+
+
+# class Country:
+#     def __init__(self, name, continent, population, phone_code, capital, cities):
+#         self._name = name
+#         self._continent = continent
+#         self._population = population
+#         self._phone_code = phone_code
+#         self._capital = capital
+#         self._cities = cities
+#
+#     def get_name(self):
+#         return self._name
+#
+#     def get_continent(self):
+#         return self._continent
+#
+#     def get_population(self):
+#         return self._population
+#
+#     def get_phone_code(self):
+#         return self._phone_code
+#
+#     def get_capital(self):
+#         return self._capital
+#
+#     def get_cities(self):
+#         return self._cities
+#
+#     def set_name(self, name):
+#         self._name = name
+#
+#     def set_continent(self, continent):
+#         self._continent = continent
+#
+#     def set_population(self, population):
+#         self._population = population
+#
+#     def set_phone_code(self, phone_code):
+#         self._phone_code = phone_code
+#
+#     def set_capital(self, capital):
+#         self._capital = capital
+#
+#     def set_cities(self, cities):
+#         self._cities = cities
